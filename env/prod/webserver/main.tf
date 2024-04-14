120a0754e4bdae60b04c6b2b7a283b16d2cb3470
Revanth <pavanrevanth97@gmail.com>
2024-04-14 23:28:09 +0000

    rectifying syntax

--- env/prod/webserver/main.tf
+++ env/prod/webserver/main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "aws_linux_ami" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-2023.4.20240401.1-kernel-6.1-x86_64"]
   }
 }
 
