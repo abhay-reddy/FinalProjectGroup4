dec9787870f868fce61e75879a8f5d59c64bbf82
Abhay Reddy <abhayankith22@gmail.com>
2024-04-14 19:07:23 -0400

    Update install_httpd.sh

--- env/staging/webserver/install_httpd.sh
+++ env/staging/webserver/install_httpd.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 echo '<h1><center> Hello There! This is the final project of ACS730 by Group 4 Members: Abhay, Revanth and Sai Chandra</center></h1>
- <img src="URL"> <img src="URL"> <img src="URL">'   >  /var/www/html/index.html
+<img src="https://web-images-acs730.s3.amazonaws.com/Abhay.png"> <img src="https://web-images-acs730.s3.amazonaws.com/Revanth.png"> <img src="https://web-images-acs730.s3.amazonaws.com/Sai.png">'   >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
